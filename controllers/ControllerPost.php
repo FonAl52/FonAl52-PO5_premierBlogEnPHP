4bb77590cbfb5fb811c10e7a57bd46df219560d8
FonAL52 <pelagialstudio@outlook.com>
2023-05-18 18:49:40 +0200

    add Post model, manager, view and feature to Create Post also add Category model and manager

--- controllers/ControllerPost.php
+++ controllers/ControllerPost.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'views/View.php';
+require_once 'models/CategoryManager.php';
 
 class ControllerPost
 
 {
   private $view;
+  private $categoryManager;
 
   public function __construct()
   {
@@ -14,16 +16,111 @@ class ControllerPost
     elseif (isset($_GET['home'])) {
       $this->home();
     }
+    elseif (isset($_GET['newPost']) && $_SESSION['role']== 1) {
+      $this->create();
+    }
+    elseif (isset($_GET['new']) && $_SESSION['role']== 1) {
+      $this->addPost();
+    }
+    else {
+      $this->home();
+    }
   }
-  public function home(){
+
+  public function home()
+  {
     // $this->_postManager = new PostManager();
     // $posts = $this->_postManager->getPosts();
     $this->view = new View('Home');
     $this->view->generate(array());
   }
 
-    
+  private function create()
+  {
+    $categories = $this->categoryId();
 
+    $this->view = new View('CreatePost');
+    $this->view->generate(array('categories' => $categories));
+  }
+
+  public function addPost()
+  {
+    $categories = $this->categoryId();
+
+    $postManager = new PostManager();
+    $newFields = array_map('htmlspecialchars', $_POST);
+    $picturePath = '';
+    $userId = $_SESSION["id"];
+
+    // Valider les données soumises
+    $errors = [];
+    if (empty($newFields['title'])) {
+      $errors['title'] = "Le titre est obligatoire";
+    }
+    if (empty($newFields['chapo'])) {
+      $errors['chapo'] = "Le chapo est obligatoire";
+    }
+    if (empty($_FILES['picture']['name'])) {
+      $errors['picture'] = "Choisissez une photo pour votre article";
+    } else {
+      // Vérifier si le fichier est une image valide
+      $allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
+      if (in_array($_FILES['picture']['type'], $allowedTypes)) {
+        // Vérifier la taille du fichier
+        $maxFileSize = 2097152; // 2 Mo
+        if ($_FILES['picture']['size'] > $maxFileSize) {
+          $errors['picture'] = 'La taille du fichier dépasse la limite autorisée.';
+        } else {
+          // Générer un nouveau nom de fichier unique pour éviter les conflits de noms
+          $fileName = uniqid('', true) . '.' . pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION);
+
+          // Déplacer le fichier téléchargé dans le dossier public/images/posts avec le nouveau nom de fichier
+          $destination = 'public/images/posts/' . $fileName;
+          if (move_uploaded_file($_FILES['picture']['tmp_name'], $destination)) {
+            $picturePath = $destination;
+          } else {
+            $errors['picture'] = "Erreur lors de l'upload de l'image";
+          }
+        }
+      } else {
+        $errors['picture'] = "Le fichier téléchargé n'est pas une image valide";
+      }
+    }
+    if (empty($newFields['content'])) {
+      $errors['content'] = "Le contenu de l'article ne peut pas être vide";
+    }
+    if (empty($newFields['categoryId'])) {
+      $errors['categoryId'] = "Merci de choisir une catégorie d'article";
+    }
+    // Si les données sont valides, créer un nouvel article et l'ajouter à la base de données
+    if (empty($errors)) {
+      $post = new Post(array());
+      $post->setTitle($newFields['title']);
+      $post->setChapo($newFields['chapo']);
+      $post->setPicture($picturePath);
+      $post->setContent($newFields['content']);
+      $post->setCategoryId($newFields['categoryId']);
+      $post->setUserId($userId);
+
+      if ($postManager->createPost($post)) {
+        $_SESSION['message'] = "Votre article a été créé avec succès !";
+        header('location:post&home');
+      } else {
+        $errors['errors'] = "Une erreur est survenue lors de la création de votre article. Veuillez réessayer ultérieurement.";
+      }
+    } else {
+      $errors['errors'] = "Des erreurs ont été détectées dans le formulaire. Veuillez les corriger et réessayer.";
+    }
+
+    // Si les données ne sont pas valides ou si la création de l'article a échoué, afficher le formulaire avec les erreurs
+    $this->view = new View('CreatePost');
+    $this->view->generate(array('errors' => $errors, 'categories' => $categories));
+  }
+
+  private function categoryId(){
+    $this->categoryManager = new CategoryManager();
+    $categories = $this->categoryManager->getCategories();
+
+    return $categories;
+  }
 }
-
- ?>
---
+++ models/PostManager.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ *
+ */
+class PostManager extends Model
+{
+
+  public function createPost($post)
+  {
+    if (!($post instanceof Post)) {
+      // Retourner une erreur ou une exception
+      return false;
+    }
+    
+    return $this->createOne('post', $post);
+  }
+
+  //gréer la fonction qui va recuperer
+  //tous les posts dans la bdd
+  public function getPosts()
+  {
+    return $this->getAll('posts', 'Post');
+  }
+
+  public function getPost($id)
+  {
+    return $this->getOne('post', 'Post', $id);
+  }
+
+  public function getAll($table)
+  {
+      $this->getBdd();
+      $data = [];
+      $req = self::$bdd->prepare('SELECT * FROM ' . $table . ' ORDER BY id DESC');
+      $req->execute();
+
+      // On récupère les données directement dans un tableau
+      while ($row = $req->fetch(PDO::FETCH_ASSOC)) {
+          $data[] = $row;
+      }
+
+      $req->closeCursor();
+
+      return $data;
+  }
+
+
+
+  private function getOne($table, $obj, $id)
+  {
+    $this->getBdd();
+    $var = [];
+    $req = self::$bdd->prepare("SELECT id, categoryId, title, chapo, picture, content, DATE_FORMAT(createdAt, '%d/%m/%Y à %Hh%imin%ss') AS createdAt, DATE_FORMAT(updatedAt, '%d/%m/%Y à %Hh%imin%ss') AS updatedAt FROM " . $table . " WHERE id = ?");
+    $req->execute(array($id));
+    while ($data = $req->fetch(PDO::FETCH_ASSOC)) {
+      $var[] = new $obj($data);
+    }
+
+    return $var;
+    $req->closeCursor();
+  }
+  
+  private function createOne($table, $obj)
+{
+    $this->getBdd();
+
+    $req = self::$bdd->prepare("INSERT INTO ".$table." (categoryId, userId, title, chapo, picture, content) VALUES (?, ?, ?, ?, ?, ?)");
+    $req->execute(array(
+        $obj->getCategoryId(),
+        $obj->getUserId(),
+        $obj->getTitle(),
+        $obj->getChapo(),
+        $obj->getPicture(),
+        $obj->getContent(),
+    ));
+    $req->closeCursor();
+    return true;
+}
+}
