6e4f4c420c67940310755f524fbcd2f86e432d85
FonAL52 <pelagialstudio@outlook.com>
2023-07-11 23:39:30 +0200

    viewConnect code update for codacy rating

--- views/viewConnect.php
+++ views/viewConnect.php
@@ -1,14 +1,15 @@
 <section class="comment-section">
 	<div class="container">
-		<?php if (isset($_SESSION['message'])) { ?>
+		<?php if (isset($_SESSION['message']) === TRUE) { ?>
 			<div class="row">
 				<div class="col-lg-12 col-md-12">
 					<div class="comment-form">
-						<div class="alert-info" role="alert"><?php echo htmlspecialchars($_SESSION['message']) ?> </div>
+						<div class="alert-info" role="alert"><?php htmlspecialchars($_SESSION['message']) ?></div>
 					</div>
 				</div>
 			</div>
 		<?php }
+
 		unset($_SESSION['message']); ?>
 		<div class="row">
 			<div class="col-lg-3 col-md-0"></div>
@@ -18,11 +19,11 @@
 						<div class="row">
 							<div class="col-sm-12">
 								<input type="email" aria-required="true" name="email" class="form-control" placeholder="Entrez votre email" aria-invalid="true" required>
-								<b class="text-danger"><?php if (isset($errors['email'])) echo htmlspecialchars($errors['email']) ?></b>
+								<b class="text-danger"><?php if (isset($errors['email'])) htmlspecialchars($errors['email']) ?></b>
 							</div>
 							<div class="col-sm-12">
 								<input type="password" aria-required="true" minlength="6" name="password" class="form-control" placeholder="Entrez votre mot de passe" aria-invalid="true" required>
-								<b class="text-danger gras"><?php if (isset($errors['password'])) echo htmlspecialchars($errors['password']) ?></b>
+								<b class="text-danger gras"><?php if (isset($errors['password'])) htmlspecialchars($errors['password']) ?></b>
 							</div>
 							<div class="col-sm-12 center-text">
 								<button class="submit-btn" type="submit" id="form-submit"><b>CONNEXION</b></button>
@@ -35,6 +36,6 @@
 				</div>
 			</div>
 		</div>
-		<b class="text-danger"><?php if (isset($errors['nom_de_l_erreur'])) echo $errors['nom_de_l_erreur'] ?></b>
+		<b class="text-danger"><?php if (isset($errors['nom_de_l_erreur'])) $errors['nom_de_l_erreur'] ?></b>
 	</div>
 </section>
