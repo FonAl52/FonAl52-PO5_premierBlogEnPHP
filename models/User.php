c9ccddac342140a09df5ff2ec29937cb7a8869f8
FonAL52 <pelagialstudio@outlook.com>
2023-07-14 03:12:27 +0200

    models code update for codacy rating

--- models/User.php
+++ models/User.php
@@ -59,61 +59,118 @@ class User
             $this->id = $id;
         }
         return $this->id;
-    }
 
+    }//end setId()
+
+
+    /**
+     * Set the firstname of the user.
+     *
+     * @param string $firstName The firstname of the user.
+     */
     public function setFirstName($firstName)
     {
         if (is_string($firstName)) {
             $this->firstName = $firstName;
         }
-    }
 
+    }//end setFirstName()
+
+
+    /**
+     * Set the lastname of the user.
+     *
+     * @param string $lastName The lastname of the user.
+     */
     public function setLastName($lastName)
     {
         if (is_string($lastName)) {
             $this->lastName = $lastName;
         }
-    }
 
+    }//end setLastName()
+
+
+    /**
+     * Set the email of the user.
+     *
+     * @param string $email The email of the user.
+     */
     public function setEmail($email)
     {
         if (is_string($email)) {
             $this->email = $email;
         }
-    }
 
+    }//end setEmail()
+
+
+    /**
+     * Set the age of the user.
+     *
+     * @param integer $age The age of the user.
+     */
     public function setAge($age)
     {
         $age = (int) $age;
         if ($age > 0) {
             $this->age = $age;
         }
-    }
 
+    }//en setAge()
+
+
+    /**
+     * Set the password of the user.
+     *
+     * @param string $password The password of the user.
+     */
     public function setPassword($password)
     {
         if (is_string($password)) {
             $this->password = $password;
         }
-    }
 
+    }//end setPassword()
+
+
+    /**
+     * Set the phone number of the user.
+     *
+     * @param integer $phone The phone number of the user.
+     */
     public function setPhone($phone)
     {
         $phone = (int) $phone;
         if ($phone > 0) {
             $this->phone = $phone;
         }
-    }
 
+    }//end setPhone()
+
+
+    /**
+     * Set the picture of the user.
+     *
+     * @param string $picture The pictue of the user.
+     */
     public function setPicture($picture)
     {
         $this->picture = $picture;
-    }
 
+    }//end setPicture()
+
+
+    /**
+     * Set the role of the user.
+     *
+     * @param integer $role The role of the user.
+     */
     public function setRole($role)
     {
         $this->role = $role;
-    }
+        
+    }//end setRole()
 
 
     // getters
@@ -126,45 +183,104 @@ class User
     public function getId()
     {
         return $this->id;
-    }
 
+    }//end getId()
+
+
+    /**
+     * Get the firstname of the user.
+     *
+     * @return string The firstname of the user.
+     */
     public function getFirstName()
     {
         return $this->firstName;
-    }
 
+    }//end getFirstName()
+
+
+    /**
+     * Get the lastname of the user.
+     *
+     * @return string The lastname of the user.
+     */
     public function getLastName()
     {
         return $this->lastName;
-    }
 
+    }//end getLastName()
+
+
+    /**
+     * Get the email of the user.
+     *
+     * @return string The email of the user.
+     */
     public function getEmail()
     {
         return $this->email;
-    }
 
+    }//end getEmail()
+
+
+    /**
+     * Get the age of the user.
+     *
+     * @return integer The age of the user.
+     */
     public function getAge()
     {
         return $this->age;
-    }
 
+    }//end getAge()
+
+
+    /**
+     * Get the password of the user.
+     *
+     * @return string The password of the user.
+     */
     public function getPassword()
     {
         return $this->password;
-    }
 
+    }//end getPassword()
+
+
+    /**
+     * Get the phone number of the user.
+     *
+     * @return integer The phone number of the user.
+     */
     public function getPhone()
     {
         return $this->phone;
-    }
 
+    }//end getPhone()
+
+
+    /**
+     * Get the picture of the user.
+     *
+     * @return string The picture of the user.
+     */
     public function getPicture()
     {
         return $this->picture;
-    }
 
+    }//end getPicture()
+
+
+    /**
+     * Get the role of the user.
+     *
+     * @return integer The role of the user.
+     */
     public function getRole()
     {
         return $this->role;
-    }
+
+    }//end getRole()
+
+    
 }
