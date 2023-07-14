0dca7bd5288b8bd747e7c9266810c9e4bdc708ba
FonAL52 <pelagialstudio@outlook.com>
2023-07-14 03:00:02 +0200

    models code update for codacy rating

--- models/Post.php
+++ models/Post.php
@@ -57,61 +57,118 @@ class Post
         if ($id > 0) {
             $this->id = $id;
         }
-    }
 
+    }//end setId()
+
+
+    /**
+     * Set the category ID of the post.
+     *
+     * @param integer $categoryId The category ID of the post.
+     */
     public function setCategoryId($categoryId)
     {
         $categoryId = (int) $categoryId;
         if ($categoryId > 0) {
             $this->categoryId = $categoryId;
         }
-    }
 
+    }//end setCategoryId()
+
+
+    /**
+     * Set the user ID of the post.
+     *
+     * @param integer $userId The user ID of the post.
+     */
     public function setUserId($userId)
     {
         $userId = (int) $userId;
         if ($userId > 0) {
             $this->userId = $userId;
         }
-    }
 
+    }//end setUserId()
+
+
+    /**
+     * Set the chapo of the post.
+     *
+     * @param string $id The chapo of the post.
+     */
     public function setChapo($chapo)
     {
         if (is_string($chapo)) {
             $this->chapo = $chapo;
         }
-    }
 
+    }//end setChapo()
+
+
+    /**
+     * Set the picture of the post.
+     *
+     * @param string $picture The picture of the post.
+     */
     public function setPicture($picture)
     {
         if (is_string($picture)) {
             $this->picture = $picture;
         }
-    }
+    
+    }//end setPicture()
 
+
+    /**
+     * Set the Title of the post.
+     *
+     * @param string $title The Title of the post.
+     */
     public function setTitle($title)
     {
         if (is_string($title)) {
             $this->title = $title;
         }
-    }
 
+    }//end setTitle()
+
+
+    /**
+     * Set the content of the post.
+     *
+     * @param string $content The content of the post.
+     */
     public function setContent($content)
     {
         if (is_string($content)) {
             $this->content = $content;
         }
-    }
+    
+    }//end setContent()
 
+
+    /**
+     * Set the date of creation of the post.
+     *
+     * @param integer $createdAt The date of creation of the post.
+     */
     public function setCreatedAt($createdAt)
     {
         $this->createdAt = $createdAt;
-    }
 
+    }//end setCreatedAt
+
+
+    /**
+     * Set the update date of the post.
+     *
+     * @param integer $updatedAt The update date of the post.
+     */
     public function setUpdatedAt($updatedAt)
     {
         $this->updatedAt = $updatedAt;
-    }
+
+    }//end setUpdatedAt
 
 
     // getters
@@ -124,45 +181,104 @@ class Post
     public function getId()
     {
         return $this->id;
-    }
 
+    }//end getId()
+
+
+    /**
+     * Get the category ID of the post.
+     *
+     * @return integer The category ID of the post.
+     */
     public function getCategoryId()
     {
         return $this->categoryId;
-    }
 
+    }//end getCategoryId()
+
+
+    /**
+     * Get the user ID of the post.
+     *
+     * @return integer The user ID of the post.
+     */
     public function getUserId()
     {
         return $this->userId;
-    }
+    
+    }//end getUserId()
 
+
+    /**
+     * Get the chapo of the post.
+     *
+     * @return string The chapo of the post.
+     */
     public function getChapo()
     {
         return $this->chapo;
-    }
 
+    }//end getChapo()
+
+
+    /**
+     * Get the picture of the post.
+     *
+     * @return string The picture of the post.
+     */
     public function getPicture()
     {
         return $this->picture;
-    }
 
+    }//end getPicture()
+
+
+    /**
+     * Get the title of the post.
+     *
+     * @return string The title of the post.
+     */
     public function getTitle()
     {
         return $this->title;
-    }
 
+    }//end getTitle()
+
+
+    /**
+     * Get the content of the post.
+     *
+     * @return string The content of the post.
+     */
     public function getContent()
     {
         return $this->content;
-    }
+    
+    }//end getContent()
 
+
+    /**
+     * Get the date of creation of the post.
+     *
+     * @return integer The date of creation of the post.
+     */
     public function getCreatedAt()
     {
         return $this->createdAt;
-    }
 
+    }//end getCreatedAt()
+
+
+    /**
+     * Get the update date of the post.
+     *
+     * @return integer The update date of the post.
+     */
     public function getUpdatedAt()
     {
         return $this->updatedAt;
-    }
+
+    }//end getUpdatedAt()
+
+
 }
